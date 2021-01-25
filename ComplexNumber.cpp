966aee66e19881662958903a502649f44f6971ec
Tomas Mejia <tomatitus2@gmail.com>
2021-01-25 02:09:17 -0500

    Added ComplexNumber.cpp

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -1,2 +1,10 @@
 #include "ComplexNumber.h"
 
+ComplexNumber::ComplexNumber(double _a, double _b) {
+    a = _a;
+    b = _b;
+}
+
+double ComplexNumber::magnitudeSquare() {
+    return 0;
+}
--- ComplexNumber.h
+++ ComplexNumber.h
@@ -1,7 +1,8 @@
 #pragma once
 
 class ComplexNumber {
-    static double a; //imaginary component
-    static double b; //real component
-    static double magnitudeSquare();
+    double a, b;
+public:
+    ComplexNumber(double a, double b);
+    double magnitudeSquare();
 };
