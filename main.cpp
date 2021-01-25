3a21f8cc0eb7503134976ba48cf1aff23557ae23
Tomas Mejia <tomatitus2@gmail.com>
2021-01-25 01:11:38 -0500

    Added Circle functionality

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
-//
-// Created by Isaac Fuenmayor on 1/25/21.
-//
+#include <SFML/Graphics.hpp>
 #include <iostream>
-using std::cout;
-using std::endl;
+#include <cmath>
 
-void cringePrint(int x=0);
+int main()
+{
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 16;
+    sf::RenderWindow window(sf::VideoMode(512,512), "tutorial", sf::Style::Close, settings);
 
-int main(){
-    cringePrint();
+    while(window.isOpen()){
+        window.clear();
+        sf::Event event;
+        while(window.pollEvent(event)){
+            switch(event.type){
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+            }
+        }
+
+        sf::Vector2<int> mouse = sf::Mouse::getPosition(window);
+
+
+        sf::CircleShape shape(event.type);
+        shape.setRadius(sqrt((mouse.x-(window.getSize().x*0.5))*(mouse.x-(window.getSize().x*0.5)) + (mouse.y-(window.getSize().y*0.5))*(mouse.y-(window.getSize().y*0.5))));
+        shape.setFillColor(sf::Color(100, 250, 50));
+        shape.setOrigin(shape.getRadius() - window.getSize().x*0.5, shape.getRadius() - window.getSize().y*0.5);
+
+        window.draw(shape);
+        window.display();
+
+    }
+
+
     return 0;
-}
-
-void cringePrint(int x){
-    cout<<"cringe"<<x<<endl;
 }
