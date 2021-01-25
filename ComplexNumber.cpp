fe5c132741591282027d168bf573712677b40d68
Isaac Fuenmayor <isaacfuenmayora@gmail.com>
2021-01-25 02:10:38 -0500

    Adding the static class to manage SFML textures

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -1,8 +1,6 @@
 #include "ComplexNumber.h"
 
-ComplexNumber::ComplexNumber(double _a, double _b) {
-    a = _a;
-    b = _b;
+ComplexNumber::ComplexNumber(double _a, double _b): a(_a),b(_b) {
 }
 
 double ComplexNumber::magnitudeSquare() {
