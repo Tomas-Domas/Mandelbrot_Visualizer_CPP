bc7d8faba01045aedbe30707837458fd1e28dee8
Isaac Fuenmayor <isaacfuenmayora@gmail.com>
2021-01-25 01:04:01 -0500

    cringe commit, testin

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 // Created by Isaac Fuenmayor on 1/25/21.
 //
 #include <iostream>
+using std::cout;
+using std::endl;
+
+void cringePrint(int x=0);
+
 int main(){
-    std::cout<<"cringe"<<std::endl;
+    cringePrint();
     return 0;
 }
 
+void cringePrint(int x){
+    cout<<"cringe"<<x<<endl;
+}
