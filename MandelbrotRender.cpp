405dd971acb9d9ce7bfc0dd50911183a55558503
Isaac Fuenmayor <isaacfuenmayora@gmail.com>
2021-01-25 21:17:45 -0500

    Fixed logic for drawRender and saveImage

--- MandelbrotRender.cpp
+++ MandelbrotRender.cpp
@@ -34,16 +34,18 @@ void MandelbrotRender::drawRender(sf::RenderWindow& window) {
 void MandelbrotRender::saveImage() { //TODO: Finish putting the image in a file, and have a list or something
     unsigned int len = size.x*size.y*4;
     Uint8* pixelArr = new Uint8[len];
-    ComplexNumber point(origin.a-(size.x*scale),origin.b-(size.y*scale)-scale);
+    ComplexNumber point(origin.a-(size.x*.5*scale),origin.b-(size.y*.5*scale)-scale);
     Color color;
     for(unsigned int i=0; i<len; i+=4, point.a+=scale){
-        if(i%(4*size.x)==0)
-            point.b+=scale;
+        if(i%(4*size.x)==0) {
+            point.a = origin.a-(size.x*.5*scale);
+            point.b += scale;
+        }
         color=Shader::linearShading(point.getNumEscapeSteps());
         pixelArr[i]  =color.r;
         pixelArr[i+1]=color.g;
         pixelArr[i+2]=color.b;
-        pixelArr[i+3]=color.a;
+        pixelArr[i+3]=255;
     }
     sf::Image img;
     img.create(size.x,size.y,pixelArr);
